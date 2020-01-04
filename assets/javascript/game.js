553503849c8027b03997ca8a000f516867c26fc6
dannitay <dannielletaylor@outlook.com>
2020-01-04 12:53:30 -0600

    setup global variables and array for js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,14 @@
-//Creating varibales to hold current and last guess. Players wins and losses//
+//Global Variables
 
-var playerGuess
-var playerWins = 0;
-var playerLosses = 0;
-var guessesLeft = 10;
+let wins=0;
+let losses=0;
+let guessesLeft=9;
+let guessesSoFar=[];
+let computerGuess=" ";
+let playerGuess=" ";
 
-//Create variables to reference back to the HTML to display the items seen by the player//
-var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text");
-var remainingText = document.getElementById("remaining-text");
-var playerguessText = document.getElementById("playerguess-text");
+//array containing all letters in the alphabet
 
-//countsdown the number of guess the player has used//
-for (i=0; i<10; i++) { 
-    console.log(i+1);
-}
-
-var secretNumber =  Math.ceil(Math.random()) * 10;
-
+let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q",
+                "r","s","t","u","v","w","x","y","z"];
 
